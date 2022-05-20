0fd37f2f68a1282f5a1603d862bd628847a987fe
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2022-05-20 10:23:51 -1000

    First upload

---
+++ I2CController.cpp
@@ -0,0 +1,68 @@
+#include "I2CController.h"
+
+I2CController *m_ptr = NULL;
+
+void hiddenISR()
+{
+  if (m_ptr == NULL) {
+    return;
+  }
+  m_ptr->pos += (digitalRead(m_ptr->pA) == digitalRead(m_ptr->pB) ? 1 : -1);
+#ifdef DBG
+  Serial.println(m_ptr->pos);
+#endif
+}
+
+I2CController::I2CController(int c, int a, int b, int addr, int mid)
+	: a0(c), pA(a), pB(b), address(addr), motID(mid)
+{
+  Motor.begin(address);
+
+  pinMode(a0, INPUT);
+  pinMode(pA, INPUT);
+  pinMode(pB, INPUT);
+  attachInterrupt(digitalPinToInterrupt(a0), hiddenISR, CHANGE);
+  
+#ifdef DBG
+  Serial.println("Codeur incremental");
+#endif
+
+  //Ensure it is off!
+  reset();
+  Motor.speed(motID, 0);
+
+  m_ptr = this;
+}
+
+I2CController::~I2CController()
+{
+  m_ptr = NULL;
+  detachInterrupt(digitalPinToInterrupt(a0));
+}
+
+void I2CController::reset()
+{
+	pos = 0;
+}
+
+int I2CController::convert(int v)
+{
+	return v + 511;
+}
+
+void I2CController::moveByTicks(int ticks, int v)
+{
+  const int nv = pos + ticks;
+  while (nv != pos) {
+    const int s = convert(pos < nv ? -v : v);
+    if ((0 <= s) && (s < 460)) {
+      Motor.speed(motID, map(s, 0, 460, -100, 0));
+    } else if ((573 < s) && (s <= 1023)) {
+      Motor.speed(motID, map(s, 573, 1023, 0, 100));
+    }
+  }
+#ifdef DBG
+  Serial.println(pos);
+#endif
+  Motor.speed(motID, 0);
+}
---
+++ I2CController.h
@@ -0,0 +1,21 @@
+#include "Grove_I2C_Motor_Driver.h"
+
+class I2CController
+{
+public:
+	explicit I2CController(int c, int a, int b, int addr, int mid);
+  ~I2CController();
+
+	void moveByTicks(int ticks, int s = 200);
+	void reset();
+
+  int a0;
+  int pA;
+  int pB;
+  int address;
+  int motID;
+  volatile int pos = 0;
+  
+protected:
+	virtual int convert(int v);
+};
