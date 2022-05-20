2b6642a87ed74342f1bc455ac2a0bdebafdc490c
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2022-05-20 10:30:39 -1000

    First upload

---
+++ BluetoothInterface.cpp
@@ -0,0 +1,135 @@
+#include <SoftwareSerial.h>
+
+BluetoothInterface::BluetoothInterface(int r, int t)
+	: rx(r), tx(t), mhc(rx, tx)
+{
+}
+
+void BluetoothInterface::nullData(char *ptr, size_t s)
+{
+  int i = 0;
+  while (i < (s+1)) {
+    ptr[i] = '\0';
+    i++;
+  }
+}
+
+int BluetoothInterface::readInt()
+{
+  int out = 0;
+  bool goOn = true;
+  while (mhc.available() && goOn) {
+    delay(LOOP_DELAY);
+    char current = mhc.read();
+    bool acceptValue = true;
+
+    //Check input
+    switch (current) {
+      case '[': {
+        acceptValue = false;
+        mhc.print("RDSB"); //ReaD Size Begun
+        break;
+      }
+      case ']': {
+        acceptValue = false;
+        goOn = false;
+        mhc.print("RDSE"); //ReaD Size Ended
+        break;
+      }
+      //In case of abort request.
+      case '%': {
+        acceptValue = false;
+        goOn = false;
+        out = 0;
+        mhc.print("ABORTED");
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+
+    //Make conversion
+    if (acceptValue) {
+      //Transform to ASCII 0-9 value
+      int v = (int)current;
+      v -= 48;
+      if (v >= 0 && 9 >= v) {
+        out *= 10;
+        out += v;
+      }
+    }
+  }
+  return out;
+}
+
+void BluetoothInterface::receiveData(int dataSize)
+{
+  if (dataSize < 1) {
+    return;
+  } else if (dataSize > DATA_SIZE_LIMIT) {
+    mhc.print("DOS");
+  }
+
+  Serial.print("Document size: ");
+  Serial.println(dataSize);
+
+  bool goOn = true;
+  int t_offset = 0;
+  int inputDataOffset = 0;
+  dataset = (char *)malloc(sizeof(char) *(dataSize+1));
+  //Null it all or we'll get artefacts
+  nullit(dataset, dataSize+1);
+
+  //Notify that's ready
+  mhc.print("RDY");
+
+  //Loop over to read the data.
+  while (goOn && inputDataOffset < (dataSize+1) && t_offset < TIMEOUT) {
+    delay(READING_DELAY);
+    if (mhc.available()) {
+      char v = mhc.read();
+      //If abort is requested, flush and stop.
+      if (v == '%') {
+        //Cleanup
+        goOn = false;
+        //Notify
+        mhc.print("ABORTED");
+      } else {
+        dataset[offset] = v;
+        offset++;
+        inputDataOffset++;
+      }
+      t_offset = 0;
+    }
+    t_offset++;
+  }
+
+  //Error checking
+  if (t_offset == TIMEOUT) {
+      //Notify
+      mhc.print("TIMEDOUT");
+  } else if (inputDataOffset == (dataSize+1)) {
+    mhc.print("WOS"); //Write Operation Succeed.
+  } else if (goOn) {
+    mhc.print("WOF"); //Write Operation Failed.
+  }
+
+  //Print the document.
+  if (goOn) {
+    printData();
+  }
+
+  //Cleanup
+  offset = 0;
+  if (dataset) {
+    /* [TODO] Print content from here. */
+    free(dataset);
+    dataset = NULL;
+  }
+}
+
+void BluetoothInterface::processData()
+{
+  receiveData(readInt());
+}
---
+++ BluetoothInterface.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#define TIMEOUT 100
+#define LOOP_DELAY 100
+#define READING_DELAY 100
+#define DATA_SIZE_LIMIT 2048
+
+/*
+ * PROTOCOL SYSTEM:
+ * 
+ * [SIZE]TEXT...
+ * -> TEXT of length SIZE is printed.
+ * 
+ * IN WAITING OF TEXT; ABORT:
+ * TEXT1... % ...TEXT2
+ * -> TEXT2 is not printed.
+ * 
+ * MESSAGING SYSTEM ON:
+ * ? : RDY   : ReaDY to print
+ * [ : DSRB  : Data Size Read Begun
+ * ] : DSRE  : Data Size Read Ended
+ * % : ABORT : operation ABORTed
+ * ? : WOS   : Write Operation Succeed
+ * ? : WOF   : Write Operation Failed
+ * ? : DOS   : Data OverSize
+ * ? : BUSY  : System is printing
+ * 
+ * MESSAGING EXAMPLE:
+ * [Connecting to MHC]
+ * CLI <- CONN -- MHC
+ * CLI -- "<" --> MHC
+ * CLI <- DSRB -- MHC
+ * CLI -- ... --> MHC
+ * CLI -- ">" --> MHC
+ * CLI <- DSRE -- MHC
+ * CLI <-- RDY -- MHC
+ * CLI -- ... --> MHC
+ * CLI <- BUSY -- MHC
+ * CLI <-- WOS -- MHC
+ *  [Printing ended]
+ */
+ 
+class BluetoothInterface
+{
+public:
+	explicit BluetoothInterface(int rx = 11, int tx = 10);
+	void processData();
+
+protected:
+	void nullData(char *ptr, size_t s);
+	int readInt();
+	void receiveData(int dataSize);
+
+private:
+	int tx;
+	int rx;
+	int offset = 0;
+	char *dataset = NULL;
+	SoftwareSerial mhc;
+};
